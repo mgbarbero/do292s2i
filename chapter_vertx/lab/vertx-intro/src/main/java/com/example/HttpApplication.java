d47681dcf50c2908a81e66ce021557a3eb84941c
anademcz <obarbero@mbts.com.ar>
2020-09-15 20:46:52 +0000

    my change

--- chapter_vertx/lab/vertx-intro/src/main/java/com/example/HttpApplication.java
+++ chapter_vertx/lab/vertx-intro/src/main/java/com/example/HttpApplication.java
@@ -16,17 +16,39 @@ public class HttpApplication extends AbstractVerticle {
   public void start() {
     
     // TODO: Create a router object
-    
+     Router router = Router.router(vertx);
+
+
     // TODO: Add router for /api/greeting here
-    
+    router.get("/api/greeting").handler(this::greeting);
+      
+
     // TODO: Add a StaticHandler for accepting incoming requests
-    
+    router.get("/*").handler(StaticHandler.create());
+
     // TODO: Create the HTTP server listening on port 8080
     
+     vertx.createHttpServer().requestHandler(router).listen(8080);
+
+
+
     System.out.println("THE HTTP APPLICATION HAS STARTED");
   }
 
   // TODO: Add method for greeting here
+ private void greeting(RoutingContext rc) {
+    String name = rc.request().getParam("name");
+    if (name == null) {
+        name = "World";
+    }
+
+    JsonObject response = new JsonObject()
+        .put("content", String.format(template, name));
+
+    rc.response()
+        .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
+        .end(response.encodePrettily());
+    }
 
   
 }
